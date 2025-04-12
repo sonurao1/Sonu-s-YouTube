9c1df5c8ea0c183634742ea52925e014260ff7b0
sonurao1 <sonurao.skr@gmail.com>
2025-04-13 03:17:00 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -19,6 +19,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.static("public"))
 
 
+//Routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
+
+
 
 export {
     app
